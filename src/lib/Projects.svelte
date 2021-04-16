5057c7920a6c9250fb78a031dc09deea98d18478
Máté Homolya <mate.homolya@gmail.com>
2021-04-16 14:21:02 -0600

    Project descriptions

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Masonry from './Masonry.svelte';
+	import { t } from '$lib/message';
+	import { language } from '$lib/stores';
 
 	// TODO fetch from firestore
 	type Project = {
@@ -8,76 +10,128 @@
 		title: string;
 		description?: string;
 	};
-	const projects: {
+	let projects: {
 		[key: string]: Project;
-	} = {
-		bari: {
-			images: ['bari-1.png'],
-			title: 'Hairdresser logo'
-		},
-		'books-teens': {
-			images: ['book-teens-1.jpeg', 'book-teens-2.jpeg'],
-			title: 'Book cover design'
-		},
-		cctv: {
-			images: ['cctv-1.png', 'cctv-2.png'],
-			title: 'CCTV-time lapse logo',
-			description:
-				'I did this project for a start-up company, that was developing a system for construction sites. They installed CCTV cameras on the construction ground and did a time lapse of the entire construction from start to finish.'
-		},
-		'ef-studio': {
-			images: ['ef-studio-1.png'],
-			title: 'EF studio',
-			description: 'Logo for video company'
-		},
-		'fruit-pattern': {
-			images: ['fruit-pattern-1.png'],
-			title: 'Fruit pattern design',
-			description: 'I made this pattern design for a company that sells breakfast cereals'
-		},
-		koncz: {
-			images: ['koncz-1.png'],
-			title: 'Photographer logo',
-			description: 'This logo is for a freelance photographer named Koncz'
-		},
-		landscape: {
-			images: ['landscape-1.png'],
-			title: 'Nighttime Landscape',
-			description: `For Easter 2020 I was assigned to make a design based on the fact that we can't celebrate Jesus's resurrection the same way as we used to. At this time, when a hug becomes a weapon and staying away from your family is an act of love, it's important to see that little thing we can still be thankful for. I hope this design makes you wonder a little, about these things!`
-		},
-		lumiere: {
-			images: ['lumiere-1.png', 'lumiere-2.png'],
-			title: 'Lumière',
-			description: 'Lumière - French for "Light"'
-		},
-		scout: {
-			images: ['scout-1.jpeg', 'scout-2.jpeg', 'scout-3.jpeg'],
-			title: 'Hungarian Scout clothing design',
-			description:
-				'My job in this project was to create a design for a T-shirt and a scouts-tie for Hungarian scouts, who will attend international events. My reference for pattern on the scouts-tie was traditional Hungarian sewing pattern. All the elements of the T-shirt design are either a scout or a Hungarian symbol.'
-		},
-		'history-animation': {
-			images: [],
-			videos: ['history-animation.mp4'],
-			title: 'Néri History Animation'
-		},
-		'mtv-intro': {
-			images: [],
-			videos: ['mtv-intro.mp4'],
-			title: 'Néri MTV Intro Animation'
-		}
 	};
+	language.subscribe((language) => {
+		projects = {
+			bari: {
+				images: ['bari-1.png'],
+				title: t('projects.bari.title', language)
+			},
+			'books-teens': {
+				images: ['book-teens-1.jpeg', 'book-teens-2.jpeg'],
+				title: t('projects.books-teens.title', language)
+			},
+			cctv: {
+				images: ['cctv-1.png', 'cctv-2.png'],
+				title: t('projects.cctv.title', language),
+				description: t('projects.cctv.description', language)
+			},
+			'ef-studio': {
+				images: ['ef-studio-1.png'],
+				title: t('projects.ef-studio.title', language),
+				description: t('projects.ef-studio.description', language)
+			},
+			'fruit-pattern': {
+				images: ['fruit-pattern-1.png'],
+				title: t('projects.fruit-pattern.title', language),
+				description: t('projects.fruit-pattern.description', language)
+			},
+			koncz: {
+				images: ['koncz-1.png'],
+				title: t('projects.koncz.title', language),
+				description: t('projects.koncz.description', language)
+			},
+			landscape: {
+				images: ['landscape-1.png'],
+				title: t('projects.landscape.title', language),
+				description: t('projects.landscape.description', language)
+			},
+			lumiere: {
+				images: ['lumiere-1.png', 'lumiere-2.png'],
+				title: t('projects.lumiere.title', language),
+				description: t('projects.lumiere.description', language)
+			},
+			scout: {
+				images: ['scout-1.jpeg', 'scout-2.jpeg', 'scout-3.jpeg'],
+				title: t('projects.scout.title', language),
+				description: t('projects.scout.description', language)
+			},
+			'history-animation': {
+				images: [],
+				videos: ['history-animation.mp4'],
+				title: t('projects.history-animation.title', language)
+			},
+			'mtv-intro': {
+				images: [],
+				videos: ['mtv-intro.mp4'],
+				title: t('projects.mtv-intro.title', language)
+			}
+		};
+	});
+
+	console.log(projects);
 </script>
 
 <Masonry class="my-12 mx-4 md:mx-6">
 	{#each Object.entries(projects) as [id, project]}
 		{#each project.images as image}
-			<img alt={project.title} src={`/projects/${image}`} />
+			<div class="detail">
+				<img alt={project.title} src={`/projects/${image}`} />
+				<div class="overlay">
+					<p class="title">{project.title}</p>
+					{#if project.description}
+						<p class="subtitle">{project.description}</p>
+					{/if}
+				</div>
+			</div>
 		{/each}
 		{#if project.videos}
 			{#each project.videos as video}
-				<video loop autoplay muted src={`/projects/${video}`} />
+				<div class="detail">
+					<video loop autoplay muted src={`/projects/${video}`} />
+					<div class="overlay">
+						<p class="title">{project.title}</p>
+						{#if project.description}
+							<p class="subtitle">{project.description}</p>
+						{/if}
+					</div>
+				</div>
 			{/each}
 		{/if}
 	{/each}
 </Masonry>
+
+<style lang="scss">
+	@import './mixins';
+	.title {
+		@include typography(headline6);
+	}
+	.subtitle {
+		@include typography(caption);
+		@apply mt-2;
+		text-overflow: ellipsis;
+	}
+	.detail {
+		position: relative;
+		text-align: left;
+		color: white;
+		& > .overlay {
+			overflow: hidden;
+			padding: 1rem;
+			background-color: rgba(0, 0, 0, 0.38);
+			opacity: 0;
+			top: 0;
+			left: 0;
+			right: 0;
+			min-height: 100%;
+			position: absolute;
+			z-index: 1;
+			transition: opacity 75ms linear;
+		}
+		&:hover > .overlay {
+			opacity: 1;
+		}
+	}
+</style>
