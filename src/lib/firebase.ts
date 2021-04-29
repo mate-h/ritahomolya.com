25ccb0248ddbe571bdf0d195493ba81b33bbe5b1
Máté Homolya <mate.homolya@gmail.com>
2021-04-29 15:20:45 -0600

    Encrypted service account cert

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,24 @@
 global.exports = {}
-import { Firestore } from '@google-cloud/firestore';
-export const firestore = new Firestore();
-
+import crypto from 'crypto';
+import { encrypted } from '../../service-account.enc';
 import admin from 'firebase-admin';
+
+const algorithm = 'aes-128-cbc';
+const decipher = crypto.createDecipheriv(
+  algorithm,
+  process.env['GOOGLE_ENCRYPTED_KEY'],
+  process.env['GOOGLE_ENCRYPTED_IV']
+);
+
+export const getDecryptedSecret = (): admin.ServiceAccount => {
+  let decrypted = decipher.update(encrypted, 'base64', 'utf8');
+  decrypted += decipher.final('utf8');
+  return JSON.parse(decrypted);
+};
+
+const serviceAccount = getDecryptedSecret();
 export const app = admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert(serviceAccount)
 });
+
+export const firestore = app.firestore();
